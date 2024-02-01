de15c8ca63812bce14b9da827dfe5b54d1ff2993
Sergiy <sergey.morenets@gmail.com>
2024-02-01 11:54:21 +0200

    Task 2.1. Use REST Assured for Spring MVC integration tests

---
+++ rest-server/src/test/java/it/discovery/web/restassured/BookControllerTest.java
@@ -0,0 +1,52 @@
+package it.discovery.web.restassured;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import it.discovery.dto.BookDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class BookControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+
+    @Test
+    @DisplayName("GET /api/books Returns single book at startup")
+    void findAll_singleBookPresent_bookReturned() {
+        Integer bookCount = given().when().get("/api/books").then().status(HttpStatus.OK)
+                .contentType(ContentType.JSON)
+                .extract().response().jsonPath().getObject("size()", Integer.class);
+        assertEquals(1, bookCount);
+    }
+
+    @Test
+    @DisplayName("POST /api/books Creates new book without required fields")
+    void save_titleIsEmpty_badRequest() {
+        BookDTO book = new BookDTO();
+        book.setAuthor("Roy Fielding");
+        book.setYear(2022);
+        book.setAmount(5);
+
+        given().body(book).contentType(ContentType.JSON)
+                .when().post("/api/books")
+                .then().status(HttpStatus.BAD_REQUEST);
+    }
+
+}
+
